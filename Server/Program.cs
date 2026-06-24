95ba9f5c4e92a53c6816f8231b9613682ad38370
mahov <64092210+bfgr@users.noreply.github.com>
2026-06-24 14:28:05 +0300

    Create Program.cs

---
+++ Server/Program.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO.Pipes;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("SERVER STARTED");
+
+        using var pipe = new NamedPipeServerStream("chat_pipe", PipeDirection.InOut);
+        pipe.WaitForConnection();
+
+        Console.WriteLine("CLIENT CONNECTED");
+
+        byte[] buffer = new byte[1024];
+
+        while (true)
+        {
+            int bytes = pipe.Read(buffer, 0, buffer.Length);
+            string msg = Encoding.UTF8.GetString(buffer, 0, bytes);
+
+            Console.WriteLine("Client: " + msg);
+
+            string response =
+                msg == "time" ? DateTime.Now.ToString() :
+                msg.StartsWith("echo ") ? msg.Substring(5) :
+                msg == "exit" ? "bye" :
+                "unknown";
+
+            byte[] data = Encoding.UTF8.GetBytes(response);
+            pipe.Write(data, 0, data.Length);
+
+            if (msg == "exit")
+                break;
+        }
+    }
+}
